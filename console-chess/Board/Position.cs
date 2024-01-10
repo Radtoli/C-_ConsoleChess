aa908958e76d8cd2cb3f2ba61fbcd3bb60152b55
Raul Duarte <raultorresoliveira@gmail.com>
2024-01-10 11:26:27 -0300

    Position Class Created

---
+++ console-chess/Board/Position.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace console_chess.Board
+{
+    internal class Position
+    {
+
+        public int row { get; set; }
+        public int column { get; set; }
+
+        public Position(int row, int column)
+        { 
+            this.row = row;
+            this.column = column;
+        }
+
+        public override string ToString()
+        {
+            return row + ", " + column;
+        }
+
+    }
+}
--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace console_chess
+﻿using console_chess.Board;
+
+
+namespace console_chess
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Position P;
+
+            P = new Position(3, 4);
+
+            Console.WriteLine("Position:" + P);
         }
     }
-}
+} 
